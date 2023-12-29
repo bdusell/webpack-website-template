9d9bf7049916d7bb3174c6c3eb7cf182381c2fbd
Brian DuSell <bdusell@gmail.com>
2023-12-29 17:13:15 +0100

    Get pug base templates working.

--- frontend/src/html/_metadata.js
+++ frontend/src/html/_metadata.js
@@ -1,8 +1,9 @@
-//- These are variables that can be used to configure the basic templates.
+// These are variables that can be used to configure the basic templates.
 const siteName = 'Webpack 2023 Demo';
 const baseUrl = 'https://example.com';
 const siteTwitterAccount = 'X';
-const favicon = require('../../assets/images/favicon.png');
+//const favicon = require('../../assets/images/favicon.png');
+const favicon = null;
 
 module.exports = {
   siteName,
