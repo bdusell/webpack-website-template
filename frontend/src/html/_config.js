084920b2b74f540832b0c9db20befc156c36d98d
Brian DuSell <bdusell@gmail.com>
2024-01-06 11:24:32 +0100

    Add favicon and preview images.
    Rename _metadata.js to _config.js.

--- frontend/src/html/_config.js
+++ frontend/src/html/_config.js
@@ -2,8 +2,7 @@
 const siteName = 'Webpack 2023 Demo';
 const baseUrl = 'https://example.com';
 const siteTwitterAccount = 'X';
-//const favicon = require('../../assets/images/favicon.png');
-const favicon = null;
+const favicon = require('../../assets/images/webpack-logo.png');
 
 module.exports = {
   siteName,
