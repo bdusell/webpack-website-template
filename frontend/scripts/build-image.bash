6b7dd26ee254dc10eea1c4ef660ab74770bd61d4
Brian DuSell <bdusell@gmail.com>
2024-01-13 19:51:47 +0100

    Fix scripts for building production frontend image.

--- frontend/scripts/build-image.bash
+++ frontend/scripts/build-image.bash
@@ -2,4 +2,4 @@ set -e
 set -u
 . scripts/variables.bash
 bash scripts/build-dev-image.bash "$@"
-DOCKER_BUILDKIT=1 docker build "$@" -t "$IMAGE":latest .
+DOCKER_BUILDKIT=1 docker build "$@" -t "$IMAGE":latest --build-arg=DEV_IMAGE_NAME="$DEV_IMAGE" .
