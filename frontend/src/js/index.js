ca2f1d6c422637d3ef0650fc8aba48044f9007a8
Brian DuSell <bdusell@gmail.com>
2024-01-13 01:55:08 +0100

    Get async/await syntax to work.

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,1 +1,17 @@
 import './common';
+
+function wait(duration, message) {
+  return new Promise((resolve) => {
+    setTimeout(() => { resolve(message); }, duration);
+  });
+}
+
+async function myFunc() {
+  const message = await wait(1000, 'hello');
+  return `${message} world`;
+}
+
+console.log('waiting...');
+myFunc(1000, 'hello world').then((message) => {
+  console.log(message);
+});
